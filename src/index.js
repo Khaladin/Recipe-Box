4610ada2c1d54b572491a8e26f66dae97d3aed06
Cory Hoban <hobancor@gmail.com>
2018-04-25 07:12:13 -0400

    removed un-needed files, added dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import NavBar from './components/navbar';
+
 ReactDOM.render(
   <Router>
-    <App />
+    <NavBar />
   </Router>
 
 
