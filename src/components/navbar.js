c9438ed629e98469659cb64e23f24d89d5321685
Cory Hoban <hobancor@gmail.com>
2018-04-26 07:10:39 -0400

    Add scss file structure

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,17 +12,17 @@ _onToggleNav = () => {
   render() {
     const {navCollapsed} = this.state;
     return(
-      <nav className="navbar navbar-default">
+      <nav className="navbar navbar-default menu">
         <div className="navbar-header">
           <a className='navbar-brand' href='/'>Your Brand</a>
           <button
             aria-expanded='false'
-            className='navbar-toggle collapsed'
+            className='navbar-toggle collapsed justify-content-end menu__button'
             onClick={this._onToggleNav}
             type='button'
           >
           <span className='sr-only'>Toggle navigation</span>
-            <span className='icon-bar'>Menu</span>
+            <span className='icon-bar menu__but'>Menu</span>
 
           </button>
         </div>
