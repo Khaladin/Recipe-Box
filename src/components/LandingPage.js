0456c613c2d63ae46e34ed5485ee7b322ccf889f
Cory Hoban <hobancor@gmail.com>
2018-05-15 06:56:02 -0400

    add search bar

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,9 +5,17 @@ class LandingPage extends Component {
     return(
       <div className="lp">
         <div className="container-fluid">
-          <h1 className="mx-auto">Your Favorite Recipies</h1>
-          <h3 className="mx-auto">All in one place</h3>
+          <h1 className="mx-auto text-center">Your Favorite Recipies</h1>
+          <h3 className="mx-auto text-center">All in one place</h3>
         </div>
+
+        <div class="input-group input-group-large">
+          <input type="text" class="form-control" placeholder="What's for Dinner?" aria-label="Recipie" aria-describedby="basic-addon2" />
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">Button</button>
+          </div>
+        </div>
+
       </div>
     )
   }
